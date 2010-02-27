6cafdc3372b59e95f93e1e9766bb361035313131
Scott Brooks <scott@scottbrooks.ca>
2010-02-27 12:36:58 -0500

    Supporting Beanstream payment profiles
    [#1182]

--- vendor/extensions/payment_gateway/app/models/gateway/beanstream.rb
+++ vendor/extensions/payment_gateway/app/models/gateway/beanstream.rb
@@ -8,6 +8,46 @@ class Gateway::Beanstream < Gateway
     ActiveMerchant::Billing::BeanstreamGateway
   end
 
+  def payment_profiles_supported?
+    true
+  end
+
+  def create_profile(creditcard, gateway_options)
+    if creditcard.gateway_customer_profile_id.nil?
+      options = options_for_create_customer_profile(creditcard, gateway_options)
+      verify_creditcard_name!(creditcard)
+      result = provider.store(creditcard, options)
+      if result.success?
+        creditcard.update_attributes(:gateway_customer_profile_id => result.params["customerCode"], :gateway_payment_profile_id => result.params["customer_vault_id"])
+      else
+        raise "Error creating profile: #{result.message}"
+      end
+    end
+  end
+  private
+  def verify_creditcard_name!(creditcard)
+    bill_address = creditcard.payments.first.payable.bill_address
+    creditcard.first_name = bill_address.firstname unless creditcard.first_name?
+    creditcard.last_name = bill_address.lastname   unless creditcard.last_name?
+  end
+
+  def options_for_create_customer_profile(creditcard, gateway_options)
+    checkout = creditcard.payments.first.payable
+    address = checkout.bill_address
+    { :email=>checkout.email,
+      :billing_address=>
+      { :name=>address.full_name,
+        :phone=>address.phone,
+        :address1=>address.address1,
+        :address2=>address.address2,
+        :city=>address.city,
+        :state=>address.state_name || address.state.abbr,
+        :country=>address.country.iso,
+        :zip=>address.zipcode
+      }
+    }.merge(gateway_options)
+  end
+
   SECURE_PROFILE_URL = 'https://www.beanstream.com/scripts/payment_profile.asp'
   SP_SERVICE_VERSION = '1.1'
   PROFILE_OPERATIONS = {:new => 'N', :modify => 'M'}
@@ -50,11 +90,15 @@ class Gateway::Beanstream < Gateway
 
     def add_credit_card(post, credit_card)
       if credit_card
-        post[:trnCardOwner] = credit_card.name
-        post[:trnCardNumber] = credit_card.number
-        post[:trnExpMonth] = format(credit_card.month, :two_digits)
-        post[:trnExpYear] = format(credit_card.year, :two_digits)
-        post[:trnCardCvd] = credit_card.verification_value
+        if credit_card.has_payment_profile?
+          post[:customerCode] = credit_card.gateway_customer_profile_id
+        else
+          post[:trnCardOwner] = credit_card.name
+          post[:trnCardNumber] = credit_card.number
+          post[:trnExpMonth] = format(credit_card.month, :two_digits)
+          post[:trnExpYear] = format(credit_card.year, :two_digits)
+          post[:trnCardCvd] = credit_card.verification_value
+        end
       end
     end
 
